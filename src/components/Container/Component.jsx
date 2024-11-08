e8411c60ada56724c23df867da66ad308dad3b64
saif9124115@gmail.com <saif9124115@gmail.com.com>
2024-11-08 17:23:27 +0500

    Component

---
+++ src/components/Container/Component.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Component = ({ children }) => {
+  return <div className="w-full max-w-7xl mx-auto">{children}</div>;
+};
+
+export default Component;
---
+++ src/components/logo.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function logo({ width = "100px" }) {
+  return <div></div>;
+}
+
+export default logo;
